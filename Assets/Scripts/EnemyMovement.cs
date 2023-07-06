3b850746110ab053f45548ac132d7946153cddf0
CodeNinjasEtobicoke <codeninjasetobicoke@outlook.com>
2023-07-06 17:56:23 -0400

    Robomania
    NOT DONE

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,6 +4,37 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    public float yForce;
+    public float xForce;
+    public float xDirection;
 
+    private Rigidbody2D enemyRigidBody;
+
+    private void Start()
+    {
+        enemyRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (transform.position.x <= -8)
+        {
+            xDirection = 1;
+            enemyRigidBody.AddForce(Vector2.right * xForce);
+        }
+        if (transform.position.x >= 8)
+        {
+            xDirection = -1;
+            enemyRigidBody.AddForce(Vector2.left * xForce);
+        }
+    }
 }
 
