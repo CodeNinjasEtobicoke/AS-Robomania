b3198bb648a299bec5938061fc620230baeb10ba
Stegosaurus1117 <122121405+Stegosaurus1117@users.noreply.github.com>
2023-07-12 17:58:46 -0400

    Robomania
    Done

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -8,6 +8,9 @@ public class EnemyMovement : MonoBehaviour
     public float xForce;
     public float xDirection;
 
+    private bool ceilingHit = false;
+    public float gravity = 1f;
+
     private Rigidbody2D enemyRigidBody;
 
     private void Start()
@@ -22,6 +25,17 @@ public class EnemyMovement : MonoBehaviour
             Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
             enemyRigidBody.AddForce(jumpForce);
         }
+
+        if (collision.gameObject.tag == "Enemy")
+        {
+            Vector2 jumpForce = new Vector2(xForce * xDirection, yForce);
+            enemyRigidBody.AddForce(jumpForce);
+        }
+
+        if (collision.gameObject.tag == "Ceiling")
+        {
+            ceilingHit = true;
+        }
     }
     private void FixedUpdate()
     {
@@ -30,10 +44,16 @@ public class EnemyMovement : MonoBehaviour
             xDirection = 1;
             enemyRigidBody.AddForce(Vector2.right * xForce);
         }
+
         if (transform.position.x >= 8)
         {
             xDirection = -1;
             enemyRigidBody.AddForce(Vector2.left * xForce);
+        }
+
+        if (ceilingHit == true)
+        {
+            enemyRigidBody.velocity = Vector3.down * gravity;      
         }
     }
 }
